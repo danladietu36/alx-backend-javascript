6070c6a4757f228dcc007b936cff40d124aa4189
Danladi Etu <danladietugithub@example.com>
2023-07-25 23:14:00 +0000

    fix bugs

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,7 +4,7 @@ export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
 
-  if (trueorFalse) {
+  if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }

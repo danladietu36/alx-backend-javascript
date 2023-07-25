dbb49029418e1475863e2cd1996e9a805d260a67
Danladi Etu <danladietugithub@example.com>
2023-07-25 22:51:58 +0000

    vavriable hoisting

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueorFalse) {
+	var task = false;
+	var task2 = true;
+
+	if (trueorFalse) {
+		let task = true;
+		let task2 = false;
+	}
+
+	return [task, task2];
+}

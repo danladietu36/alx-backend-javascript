5618c2155be61e69f4d483b77c46012027ab89e9
Danladi Etu <danladietugithub@example.com>
2023-07-27 21:38:05 +0000

    class with method

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,8 +5,7 @@ export default class Car {
     this._color = color;
   }
 
-
   cloneCar() {
-    return this.constructor(this._brand, this._motor, this._color);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }

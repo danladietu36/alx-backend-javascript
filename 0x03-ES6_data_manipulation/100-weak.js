bad3610433b9a5124044c46b70fc794b970a165a
Danladi Etu <danladietugithub@example.com>
2023-08-01 22:37:14 +0000

    added

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,8 @@
-// https://stackoverflow discussed this
-
 export const weakMap = new WeakMap();
 
+// https://stackoverflow.com/questions/29413222/what-are-the-actual-uses-of-es6-weakmap
 export function queryAPI(endpoint) {
-  let no_called = 0;
-
+  let called = 0;
   if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
   weakMap.set(endpoint, called + 1);
   if (called + 1 >= 5) throw new Error('Endpoint load is high');

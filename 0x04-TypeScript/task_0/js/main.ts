babee61cff41a7bc86a3f1373e6971e2ccfd6272
Danladi Etu <danladietugithu@gmail.com>
2023-09-19 23:46:20 -0700

    added

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,1 +1,23 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: Number;
+    location: string;
+}
+
+const firstStudent: Student = {
+    firstName: "Dan",
+    lastName: "Etu",
+    age: 32,
+    location: "Abuja",
+};
+
+const secondStudent: Student = {
+    firstName: "Omachonu",
+    lastName: "Wada",
+    age: 31,
+    location: "Abuja",
+};
+
+const studentsList: Array<Student> = [firstStudent, secondStudent];
 
